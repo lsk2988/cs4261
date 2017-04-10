8097c25103fcc8e0a7fcf5fae361d1caa71f7818
sli446 <shuangke94@gatech.edu>
2017-04-09 22:31:17 -0400

    post project

--- WeProject2/app/src/main/java/com/example/shuangke/weproject2/ChemistryProjectDetail.java
+++ WeProject2/app/src/main/java/com/example/shuangke/weproject2/ChemistryProjectDetail.java
@@ -7,15 +7,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class BusinessProjectDetails extends AppCompatActivity {
+public class ChemistryProjectDetail extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_business_project_details);
+        setContentView(R.layout.activity_chemistry_project_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        getSupportActionBar().setTitle("Project Details");
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
--- WeProject2/app/src/main/java/com/example/shuangke/weproject2/PostMyProject.java
+++ WeProject2/app/src/main/java/com/example/shuangke/weproject2/PostMyProject.java
@@ -1,18 +1,29 @@
 package com.example.shuangke.weproject2;
 
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.app.DatePickerDialog;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PostMyProject extends Fragment {
-
+public class PostMyProject extends DialogFragment implements DatePickerDialog.OnDateSetListener {
 
     public PostMyProject() {
         // Required empty public constructor
@@ -22,8 +33,67 @@ public class PostMyProject extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_post_my_project, container, false);
+
+
+
+        TextView from = (TextView) view.findViewById(R.id.fromDate);
+
+
+        from.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+                DialogFragment picker = new PostMyProject();
+                picker.show(getFragmentManager(), "datePicker");
+            }
+        });
+        final TextView to = (TextView) view.findViewById(R.id.toDate);
+        to.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+                DialogFragment picker = new PostMyProject();
+                picker.show(getFragmentManager(), "datePicker");
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post_my_project, container, false);
+
+        Button post = (Button)view.findViewById(R.id.post);
+        post.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), HomePage.class);
+                startActivity(intent);
+            }
+        });
+        return view;
+
+    }
+
+
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+// Use the current date as the default date in the picker
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+// Create a new instance of DatePickerDialog and return it
+        return new DatePickerDialog(getActivity(),this, year, month, day);
+    }
+
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        Calendar c = Calendar.getInstance();
+        c.set(year, month, day);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String formattedDate = sdf.format(c.getTime());
+
     }
 
 }
+
